9fbaf03ff7fdce697eee52092b54e0939aa932f3
AmosRM <88614465+AmosRM@users.noreply.github.com>
2026-05-07 14:23:21 +0800

    Add Cardline static pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/cardline/privacy",
+        destination: "/cardline/privacy.html",
+      },
+      {
+        source: "/cardline/support",
+        destination: "/cardline/support.html",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
